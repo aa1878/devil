ca432ff25820f29703299d32d8acd4c2b2f3efa2
aa1878 <36786948+aa1878@users.noreply.github.com>
2020-07-05 17:48:25 +0000

    Update javhd69.py

--- javhd69.py
+++ javhd69.py
@@ -618,32 +618,3 @@ if __name__ == '__main__':
 	login()
 	
 	
-                                              cek = open("out/target_baru.txt", "a")
-				                                                                                                   cek.write("ID:" +user+ " Pw:" +pass9+"\n")
-				                                                                                                   cek.close()
-				                                                                                                   cekpoint.append(user+pass9)	
-																												   	
-											                               
-		except:
-			pass
-		
-	p = ThreadPool(30)
-	p.map(main, id)
-	print "\033[1;96m⊱⋕⊰══════════════════════════════════════════⊱⋕⊰" 
-	print '\033[1;96m[\033[1;97m✓\033[1;96m] \033[1;92mProcess Has Been Completed \033[1;97m....'
-	print"\033[1;96m[+] \033[1;92mTotal OK/\x1b[1;93mCP \033[1;91m: \033[1;92m"+str(len(oks))+"\033[1;97m/\033[1;93m"+str(len(cekpoint))
-	print("\033[1;96m[+] \033[1;92mCP File Has Been Saved \033[1;91m: \033[1;97mout/cp.txt")
-	raw_input("\n\033[1;96m[\033[1;97mBack\033[1;96m]")
-	menu()
-
-if __name__ == '__main__':
-	login()
-	
-	
-m]")
-	menu()
-
-if __name__ == '__main__':
-	login()
-	
-	
