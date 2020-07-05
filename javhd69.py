24ce3d9352c9260a224e0ba93d53441383302fa6
aa1878 <36786948+aa1878@users.noreply.github.com>
2020-07-05 13:15:03 +0000

    Update javhd69.py

--- javhd69.py
+++ javhd69.py
@@ -135,11 +135,11 @@ vulnot = "\033[31mNot Vuln"
 vuln = "\033[32mVuln"
 
 os.system("clear")
-jalan('\033[1;96m[✾]\x1b[1;91mWELOME TO MY SCRIPT
-jjalan('\033[1;96m[✾]\x1b[1;91mHTTPS:XNXX.COM LINK HIBURAN BIAR TIDAK STRES
-jalan('\033[1;96m[✾]\x1b[1;91mMASUKAN KATA SANDI SCRIPTNYA YA ANJING
-jalan('\033[1;96m[✾]\x1b[1;91mKALIAN BAU MEMEK ANJING
-jalan('\033[1;96m[✾]\x1b[1;91mJANGAN LUPA MAKAN/TIDUR NANTI LU MATI
+jalan('\033[1;96m[✾]\x1b[1;91mWELOME TO MY SCRIPT\x1b[1;96m[✾]' )
+jjalan('\033[1;96m[✾]\x1b[1;91mHTTPS:XNXX.COM LINK HIBURAN BIAR TIDAK STRES\x1b[1;96m[✾]' )
+jalan('\033[1;96m[✾]\x1b[1;91mMASUKAN KATA SANDI SCRIPTNYA YA ANJING\x1b[1;96m[✾]' )
+jalan('\033[1;96m[✾]\x1b[1;91mKALIAN BAU MEMEK ANJING\x1b[1;96m[✾]' )
+jalan('\033[1;96m[✾]\x1b[1;91mJANGAN LUPA MAKAN/TIDUR NANTI LU MATI\x1b[1;96m[✾]' )
 jalan('\033[1;47m                \033[1;31mUSER-JAV                  \033[1;0m     ') 
 jalan('⊱⊹⊰━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━⊱⊹⊰') 
 jalan('\033[1;91m      \033[1;91m ENTER TOOL USERNAME AND PASSWORD \033[1;0m     ') 
